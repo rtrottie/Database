eed3445250c66a48ced903f22b705e5d4624124e
ryan <themeisme@gmail.com>
2016-09-29 13:37:05 -0600

    do endponts too

--- AddDB.py
+++ AddDB.py
@@ -26,6 +26,8 @@ def delete(database, fs, id):
     :param id: ObjectId
     :return: None
     """
+    single_keys = ['min1', 'min2']
+    multiple_keys = ['mep1', 'mep2', 'mepc']
     if type(id) == type('str') or type(id) == type(u'str'):
         id = ObjectId(id)
     run = database.find_one({'_id' : id})
@@ -38,13 +40,24 @@ def delete(database, fs, id):
                 pass
             else:
                 print('Failed to Delete ' + f)
-    if 'min1' in run:
-        delete(database, fs, run['min1'])
-    if 'min2' in run:
-        delete(database, fs, run['min2'])
+    for key in single_keys:
+        if key in run:
+            delete(database, fs, run[key])
+    for key in multiple_keys:
+        if key in run:
+            for run_id in run[key]:
+                delete(database, fs, run_id)
+
     database.delete_one({'_id' : id})
 
-
+def add_file(fs, filepath, filename=''):
+    if filename.upper() not in database_cfg.compressed_files:
+        with open(filepath, 'rb') as f:
+            fileID = fs.put(f)
+    else:
+        with open(Database_Tools.compress(filepath)) as f:
+            fileID = fs.put(f)
+    return fileID
 
 def entry_exists(database, info):
     """
@@ -173,21 +186,54 @@ def add_charged_defect(collection, material, directory, other_info, other_files=
         other_info['defect_charge'] = charge
         add_dir(collection, material, os.path.join(directory,dir), other_info, other_files + [('locpot', os.path.join(directory, dir, 'LOCPOT'))])
 
-
 def add_MEP(collection, material, directory, other_info, other_files=[]):
-    print('Adding a Charged defect, must be in charged defect cell or directory with ... n3 n2 n1 0 1 2 3 ...')
-    if os.path.exists(os.path.join(directory, 'INCAR')):  # is run directory
-        dirs = [('', int(other_info['defect_charge'][0]))]
-    else:  # must go to children ... n3 n2 n1 0 1 2 3 ... to find runs
+    (db, fs, client) = load_db()
+    document = {}
+    print('Adding a Charged defect, must be in meps directory with mep1 mep2 mepc')
+    main_dirs = [('mep1', 1), ('mep2', 2), ('mepc', 'c')]
+    for main_dir, side in main_dirs:
+        print(main_dir)
+        document[main_dir] = []
+        if main_dir == 'mep1' or main_dir == 'mep2':
+            vasprun_file = os.path.join(main_dir, 'MEP.xml')
+            v = Vasprun(vasprun_file)
+            fileID = add_file(fs, vasprun_file, 'vasprun')
+            document['vasprun_{}'.format(side)] = fileID
+        else:
+            for i in range(2):
+                document['POSCAR_{}'.format(i)] = Poscar.from_file(os.path.join(main_dir, 'POSCAR.1')).as_dict()
         dirs = []
-        for dir in [name for name in os.listdir(directory) if os.path.isdir(name)]:  # for every directory
-            dirs.append((dir, int(dir))) # n is used in place of - for filesystem compatibility make sure it is a valid dir
-    for (dir, stage) in dirs:
-        print(dir)
-        other_info['MEP_Position'] = stage
-        other_info['MEP_other'] = '...'
-        add_dir(collection, material, os.path.join(directory,dir), other_info, other_files + [('locpot', os.path.join(directory, dir, 'LOCPOT'))])
-
+        for dir in [name for name in os.listdir(os.path.join(directory, main_dir)) if os.path.isdir(os.path.join(directory, main_dir, name))]:  # for every directory
+            try:
+                dirs.append((os.path.join(main_dir, dir), int(dir))) # n is used in place of - for filesystem compatibility make sure it is a valid dir
+            except:
+                pass
+        for (dir, stage) in dirs:
+            print(dir)
+            try:
+                other_info['MEP_position'] = stage
+                other_info['MEP_main_dir'] = main_dir
+                other_info['MEP_side'] = side
+                if os.path.exists(os.path.join(directory, dir, 'vasprun.xml')):
+                    id = add_dir(collection, material, os.path.join(directory, dir), other_info=other_info, other_files=other_files).inserted_id
+                document[main_dir].append(id)
+            except Exception as e:
+                for k, v in document.items():
+                    if type(v) == ObjectId:
+                        if 'vasprun' in k:
+                            fs.delete(v)
+                        else:
+                            delete(db[collection], fs, v)
+                if 'mep' in k:
+                    for run_id in v:
+                        try:
+                            delete(db[collection], fs, run_id)
+                        except:
+                            pass
+                print('Failed to Add MEP run, all files have been deleted')
+                raise e
+    other_info.update(document)
+    db[collection].insert_one(document)
 
 def add_nupdown_convergence(collection, material, directory, other_info={}, other_files=[], check_convergence=True):
     dirs = []
@@ -250,16 +296,9 @@ def add_vasp_run(collection, material, incar, kpoints, potcar, contcar, outcar, 
         info['files'] = []
         for (filename, filepath) in files:
             if os.path.exists(filepath) and os.path.getsize(filepath) > 0:
-                if filename.upper() not in database_cfg.compressed_files:
-                    with open(filepath) as f:
-                        fileID = fs.put(f)
-                    info[filename] = fileID
-                    info['files'].append(filename)
-                else:
-                    with open(Database_Tools.compress(filepath)) as f:
-                        fileID = fs.put(f)
-                    info[filename] = fileID
-                    info['files'].append(filename)
+                fileID = add_file(fs, filepath, filename)
+                info[filename] = fileID
+                info['files'].append(filename)
             else:
                 info[filename] = 'none'
         result = collection.insert_one(info)
@@ -304,8 +343,9 @@ def add_gsm_run(collection, material, directory, other_info={}, other_files = []
     return collection.insert_one()
 
 def add_dir(collection, material, directory, other_info={}, other_files=[], force=False):
+    print(other_files)
     poscar = os.path.join(directory, 'CONTCAR') if os.path.exists(os.path.join(directory, 'CONTCAR')) and os.path.getsize(os.path.join(directory, 'CONTCAR')) > 0 else os.path.join(directory, 'POSCAR')
-    other_files = list(map(lambda n, x: (n, os.path.join(directory, x)), other_files))
+    other_files = [ (n, os.path.join(directory, x)) for n, x in other_files ]
     return add_vasp_run(collection, material, os.path.join(directory, 'INCAR'), os.path.join(directory, 'KPOINTS'),
                         os.path.join(directory, 'POTCAR'), os.path.join(directory, poscar), os.path.join(directory, 'OUTCAR'),
                         os.path.join(directory, 'vasprun.xml'), other_info=other_info, other_files=other_files, force=force)
@@ -326,6 +366,8 @@ if __name__ == '__main__':
     parser.add_argument('-c', '--charged_defect', help='Specifies this is a Charged Defect run both this flag and label must be set',
                         action='store_true')
     parser.add_argument('--fn', '--force-nupdown', help='Force adding all NUPDOWN',
+                        action='store_true')
+    parser.add_argument('--mep', '--minimum-energy-pathway', help='add MEP run',
                         action='store_true')
     parser.add_argument('--cc', '--check_convergence', help='Don"t check convergence',
                         action='store_false')
@@ -360,7 +402,7 @@ if __name__ == '__main__':
         raise Exception('charged_defect must be specified twice')
     elif args.nupdown != ('convergence_type' in tags and tags['convergence_type'][0] == 'nupdown'):
         raise Exception('must specify -n flag and correctly label DATABASE file')
-    elif 'ICHAIN' in Incar.from_file('INCAR') and Incar.from_file('INCAR')['ICHAIN'] == 2:
+    elif os.path.exists('INCAR') and 'ICHAIN' in Incar.from_file('INCAR') and Incar.from_file('INCAR')['ICHAIN'] == 2:
         if tags['ts_type'][0] != 'dimer':
             raise Exception('Dimer run, dimer ts_type must be set and ICHAIN = 2 in INCAR')
         add_dimer_run('database', material, os.path.abspath('.'), other_info=tags, other_files=other_files)
@@ -375,6 +417,8 @@ if __name__ == '__main__':
             raise Exception('Must say either y or n')
     elif os.path.exists('nupdown') and args.fn:
         add_nupdown_convergence('database', material, os.path.abspath('.'), tags, other_files)
+    elif args.mep:
+        add_MEP('database', material, os.path.abspath('.'), tags, other_files=other_files)
     else:
         poscar = 'CONTCAR' if os.path.exists('CONTCAR') and os.path.getsize('CONTCAR') > 0 else 'POSCAR'
         add_vasp_run('database', material, 'INCAR', 'KPOINTS', 'POTCAR', poscar, 'OUTCAR', 'vasprun.xml', tags, other_files, check_convergence=args.cc)
