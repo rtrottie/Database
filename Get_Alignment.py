6d532a069c9f00c7a1269bfac5f840f7d063aae9
Ryan Trottier <themeisme@gmail.com>
2017-10-06 08:37:54 -0600

    easy swithch optimizer

--- Get_Alignment.py
+++ Get_Alignment.py
@@ -8,6 +8,7 @@ import numpy as np
 import database_cfg
 import AddDB
 import os
+from time import sleep
 import tempfile
 
 def parse_vline(vline):
@@ -47,19 +48,21 @@ def parse_sxdefectalign(lines):
 try: input = raw_input
 except NameError: pass
 base_match = {
-    'material' : 'cdte',
+    'material' : {'$all': ['mnte', 'zincblende']},
+    'antiferromagnetic_label' : 'afiii',
     'locpot' : {'$exists' : True},
     'defect' : {'$exists' : False},
     'converged' : True,
-    'incar.LHFCALC' : True
+    'incar.METAGGA' : 'Scan'
 }
 match_criteria = {
-    'material' : 'cdte',
+    'material' : {'$all': ['mnte', 'zincblende']},
+    'antiferromagnetic_label' : 'afiii',
     'locpot' : {'$exists' : True},
     'defect' : {'$nin' : [],
                 '$exists' : True},
     'alignment.vline' : {'$exists' : False},
-    'incar.LHFCALC' : True
+    'incar.METAGGA' : 'Scan'
 }
 
 db, fs, client = AddDB.load_db()
@@ -74,10 +77,11 @@ os.mkdir(tmp_dir)
 for run in runs:
     print(str(run['defect']) + ' ' + str(run['defect_charge']))
     locpot = Database_Tools.get_file(fs, run['locpot'], fix_as='LOCPOT', fix_args=Poscar.from_dict(run['poscar']))
-    avg = np.array(0)
+    # avg = np.array(0)
     vline_energies = []
+    vline_z = []
     command = ['sxdefectalign', '--vasp',
-               '--average', '1',
+               '--average', '5',
                # '-a' + str(i+1),
                '--vref', base_locpot,
                '--vdef', locpot,
@@ -88,18 +92,18 @@ for run in runs:
     process = subprocess.Popen(command, stdout=subprocess.PIPE)
     process.wait()
     output = process.stdout.readlines()
-    print(output)
     alignment = parse_sxdefectalign(output)
 
     for i in range(3):
         vline = parse_vline('vline-eV-a{}.dat'.format(i))
         # plt.plot(vline['z'], vline['V']['short_range'], label=str(i))
         vline_energies.append(list(vline['V']['short_range']))
-        avg = avg + vline['V']['short_range']
-    avg = avg/3
-    vline_energies.append(list(avg))
+        vline_z.append(list(vline['z']))
+        # avg = avg + vline['V']['short_range']
+    # avg = avg/3
+    # vline_energies.append(list(avg))
     alignment['vline'] = vline_energies
-    alignment['vline_axis'] = vline['z']
+    alignment['vline_axis'] = vline_z
     db.database.update_one({'_id' : run['_id']},
                            {'$set': {'alignment' : alignment}})
     os.remove(locpot)
@@ -107,42 +111,42 @@ for run in runs:
 match_criteria['alignment.vline']  = {'$exists' : True}
 match_criteria['alignment.align'] = {'$exists' : False}
 runs = db.database.find(match_criteria)
+plt.ion()
 for run in runs:
     print(str(run['defect']) + ' ' + str(run['defect_charge']))
-    z = run['alignment']['vline_axis']
     vline = run['alignment']['vline']
     for i in range(len(vline)):
         if i == 3:
             label = 'avg'
         else:
             label = str(i)
-        plt.plot(z, vline[i], label=label, linewidth=3)
+        plt.plot(run['alignment']['vline_axis'][i], vline[i], label=label, linewidth=3)
     plt.legend()
     plt.show()
+    plt.pause(1)
     average_is = input('Give ints to fit average around (or delete / d) (or pass ): \n --> ')
+    plt.cla()
     if average_is == 'delete' or average_is == 'd':
         db.database.delete_one({'_id' : run['_id']})
     else:
         average_is = average_is.split()
-        if len(average_is) == 2:
-            potential_i = 3
+        if average_is[0][0] == 'p':
+            pass
         elif len(average_is) == 3:
             potential_i = int(average_is[2])
         else:
-            raise Exception('Need to Provide exactly two numbers')
-        if average_is[0][0] == 'p':
-            pass
-        else:
-            bot = np.searchsorted(z, float(average_is[0]))
-            top = np.searchsorted(z, float(average_is[1]))
-            mean = np.mean(vline[potential_i][bot:top])
-            std = np.std(vline[potential_i][bot:top])
-            alignment = run['alignment']
-            alignment['align'] = mean
-            alignment['alignment_quality'] = std
-            print('Average  : ' + str(mean) + '\nStd.Dev. : ' + str(std))
-            db.database.update_one({'_id' : run['_id']},
-                                   {'$set': {'alignment' : alignment}})
+            raise Exception('Need to Provide exactly three numbers')
+        z = run['alignment']['vline_axis'][potential_i]
+        bot = np.searchsorted(z, float(average_is[0]))
+        top = np.searchsorted(z, float(average_is[1]))
+        mean = np.mean(vline[potential_i][bot:top])
+        std = np.std(vline[potential_i][bot:top])
+        alignment = run['alignment']
+        alignment['align'] = mean
+        alignment['alignment_quality'] = std
+        print('Average  : ' + str(mean) + '\nStd.Dev. : ' + str(std))
+        db.database.update_one({'_id' : run['_id']},
+                               {'$set': {'alignment' : alignment}})
 
 
 
@@ -154,7 +158,6 @@ runs = db.database.find(match_criteria).sort('defect', pymongo.ASCENDING)
 for run in runs:
     print(str(run['defect']) + ' ' + str(run['defect_charge']))
     locpot = Database_Tools.get_file(fs, run['locpot'], fix_as='LOCPOT', fix_args=Poscar.from_dict(run['poscar']))
-    avg = np.array(0)
     command = ['sxdefectalign', '--vasp',
                '--average', '1',
                '--vref', base_locpot,
