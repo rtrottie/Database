13619f8eeb447637de76781c9e719e6772a8ef68
Ryan Trottier <themeisme@gmail.com>
2018-05-30 08:11:16 -0600

    if in right place

--- AddDB.py
+++ AddDB.py
@@ -667,10 +667,8 @@ if __name__ == '__main__':
             pass
         else:
             raise Exception('Must say either y or n')
-    if args.nupdown and 'convergence_type' in tags and tags['convergence_type'][0] == 'nupdown':
-        add_nupdown_convergence('database', material, os.path.abspath('.'), tags, other_files=other_files)
 
-    elif 'ts_type' in tags and ('pc' in tags['ts_type'] or 'plane_constrained' in tags['ts_type']):
+    if 'ts_type' in tags and ('pc' in tags['ts_type'] or 'plane_constrained' in tags['ts_type']):
         files = [x for x in os.listdir() if '.e' in x]
         files.sort()
         with open(files[-1]) as f:
@@ -683,6 +681,8 @@ if __name__ == '__main__':
                 else:
                     raise Exception('Did not Select y/yes to add')
 
+    if args.nupdown and 'convergence_type' in tags and tags['convergence_type'][0] == 'nupdown':
+        add_nupdown_convergence('database', material, os.path.abspath('.'), tags, other_files=other_files)
     elif args.charged_defect and 'charged_defect' in tags['labels']:
         add_charged_defect('database', material, os.path.abspath('.'), tags, other_files=other_files, check_convergence=args.cc, ignore_unconverged=args.ignore_unconverged)
     elif args.charged_defect != 'charged_defect' in tags['labels']: # one or the other is established
